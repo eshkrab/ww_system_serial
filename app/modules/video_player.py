0f5017d7f8774f946889e2b63b94fd7ecd2d9bea
rina shkrabova <eshkrab@gmail.com>
2023-05-19 11:55:28 -0700

    videoplayer refactored

--- app/modules/video_player.py
+++ app/modules/video_player.py
@@ -7,7 +7,7 @@ import threading
 import numpy as np
 from enum import Enum
 from collections import deque
-from typing import Callable
+from typing import Callable, Optional, List, Dict
 
 class VideoPlayerState(Enum):
     PLAYING = 1
@@ -20,40 +20,48 @@ class VideoPlayerMode(Enum):
     STOP = 3
 
 class VideoPlayer:
-    def __init__(self, ws_queue, video_dir: str, fps: int = 30, display_callback: Callable[[np.array], None] = None):
+    def __init__(self, ws_queue, video_dir: str, fps: int = 30, 
+                 display_callback: Optional[Callable[[np.array], None]] = None):
         self.video_dir = video_dir
         self.ws_queue = ws_queue
         self.fps = fps
-        self.state = VideoPlayerState.PLAYING
+        self.state = VideoPlayerState.STOPPED
         self.mode = VideoPlayerMode.REPEAT
         self.current_video = None
-        self.playlist = []
+        self.playlist: List[Dict[str, str]] = []
         self.playlist_path = os.path.join(video_dir, "playlist.json")
         self.lock = threading.Lock()
         self.current_video_index = 0
+        self.display_callback = display_callback
 
-
-        self.display_callback = display_callback if display_callback is not None else None
-        self.populate_video_queue()
+        self.load_playlist()
 
     def play(self):
         with self.lock:
-            self.state = VideoPlayerState.PLAYING
-            if not hasattr(self, "playback_thread") or not self.playback_thread.is_alive():
-                self.playback_thread = threading.Thread(target=self.playback_loop)
-                self.playback_thread.start()
+            if self.state != VideoPlayerState.PLAYING:
+                self.state = VideoPlayerState.PLAYING
+                if not hasattr(self, "playback_thread") or not self.playback_thread.is_alive():
+                    self.playback_thread = threading.Thread(target=self.playback_loop)
+                    self.playback_thread.start()
 
     def stop(self):
         with self.lock:
-            self.state = VideoPlayerState.STOPPED
-            self.playlist.clear()
-            self.current_video = None
-            if self.playback_thread and self.playback_thread.is_alive():
-                self.playback_thread.join()
+            if self.state != VideoPlayerState.STOPPED:
+                self.state = VideoPlayerState.STOPPED
+                self.playlist.clear()
+                self.current_video = None
+                if self.playback_thread and self.playback_thread.is_alive():
+                    self.playback_thread.join()
 
     def pause(self):
         with self.lock:
-            self.state = VideoPlayerState.PAUSED
+            if self.state != VideoPlayerState.PAUSED:
+                self.state = VideoPlayerState.PAUSED
+
+    def resume(self):
+        with self.lock:
+            if self.state == VideoPlayerState.PAUSED:
+                self.play()
 
     def next_video(self):
         with self.lock:
@@ -69,40 +77,30 @@ class VideoPlayer:
         with self.lock:
             self.load_video(self.current_video_index)
 
-    def load_video(self, index):
+    def load_video(self, index: int):
         with self.lock:
             self.current_video_index = index
             self.current_video = cv2.VideoCapture(self.playlist[self.current_video_index]['video_path'])
 
-    def populate_video_queue(self):
+    def load_playlist(self):
         if not os.path.exists(self.playlist_path):
             self.create_playlist_file()
-
+        
         with open(self.playlist_path, "r") as f:
             playlist = json.load(f)
-
         self.mode = VideoPlayerMode[playlist["mode"].upper()]
-
-        for video_path in playlist["playlist"]:
-            item_data = {
-                "video_path": os.path.join(self.video_dir, video_path),
-                "playback_mode": self.mode,
-            }
-            self.playlist.append(item_data)
+        self.playlist = [
+            {"video_path": os.path.join(self.video_dir, video_path), "playback_mode": self.mode}
+            for video_path in playlist["playlist"]
+        ]
             
- 
     def create_playlist_file(self):
-        video_files = (
-            glob.glob(os.path.join(self.video_dir, "*.mp4"))
-            + glob.glob(os.path.join(self.video_dir, "*.mov"))
-            + glob.glob(os.path.join(self.video_dir, "*.avi"))
-        )
-
+        video_files = glob.glob(os.path.join(self.video_dir, "*"))
+        video_files = [file for file in video_files if file.endswith((".mp4", ".mov", ".avi"))]
         playlist = {
-            "mode": self.mode.name,  # setting to current mode of the player
-            "playlist": [os.path.basename(video_file) for video_file in video_files],
+            "mode": self.mode.name,
+            "playlist": [os.path.basename(video_file) for video_file in video_files]
         }
-
         with open(self.playlist_path, "w") as f:
             json.dump(playlist, f, indent=4)
 
@@ -116,9 +114,6 @@ class VideoPlayer:
                 continue
 
             elif self.state == VideoPlayerState.PLAYING:
-                if not self.playlist:
-                    self.populate_video_queue()
-
                 if not self.current_video and self.playlist:
                     self.load_video(self.current_video_index)
 
@@ -135,5 +130,5 @@ class VideoPlayer:
                             self.next_video()
                         elif self.mode == VideoPlayerMode.STOP:
                             self.stop()
-
             time.sleep(1 / self.fps)
+
