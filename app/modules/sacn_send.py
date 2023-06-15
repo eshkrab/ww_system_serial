666b9ebeb5a16f02b182089d323e38ed41a84c28
rina shkrabova <eshkrab@gmail.com>
2023-06-15 08:28:31 -0700

    indentation

--- app/modules/sacn_send.py
+++ app/modules/sacn_send.py
@@ -32,8 +32,7 @@ class SacnSend:
     def send_sacn_data(self, data: List[List[int]]):
         for i in range(len(data)):
             # scale data by brightness
-            #  brightness = self.brightness            #  scaled_data = [round(brightness * byte) for byte in data[i]]
-             scaled_data = [round(self.brightness / 255 * byte) for byte in data[i]]
+            scaled_data = [round(self.brightness / 255 * byte) for byte in data[i]]
             self.sender[i+1].dmx_data = scaled_data
 
     #  def send_sacn_data(self, data: List[List[int]]):
