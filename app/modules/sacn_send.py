467de987287cb00753835f51ece56cc1c721465f
rina shkrabova <eshkrab@gmail.com>
2023-06-14 19:34:08 -0700

    split into universes different

--- app/modules/sacn_send.py
+++ app/modules/sacn_send.py
@@ -18,20 +18,35 @@ class SacnSend:
         self.sender.start()
         atexit.register(self.sender.stop)
 
+
     def convert_frame_to_sacn_data(self, frame: np.array) -> List[List[int]]:
         # Convert WW animation frame to sACN data format
         dmx_data = []
         for i in range(0, len(frame), 510):
             chunk = frame[i:i+510]
-            chunk_data = array.array('B')
-            for j in range(0, len(chunk), 3):
-                chunk_data.append(chunk[j])
-            dmx_data.append(chunk_data)
+            dmx_data.append(list(chunk))
         return dmx_data
+
 
     def send_sacn_data(self, data: List[List[int]]):
         for i in range(len(data)):
             self.sender[i+1].dmx_data = data[i]
+
+
+    #  def convert_frame_to_sacn_data(self, frame: np.array) -> List[List[int]]:
+    #      # Convert WW animation frame to sACN data format
+    #      dmx_data = []
+    #      for i in range(0, len(frame), 510):
+    #          chunk = frame[i:i+510]
+    #          chunk_data = array.array('B')
+    #          for j in range(0, len(chunk), 3):
+    #              chunk_data.append(chunk[j])
+    #          dmx_data.append(chunk_data)
+    #      return dmx_data
+    #
+    #  def send_sacn_data(self, data: List[List[int]]):
+    #      for i in range(len(data)):
+    #          self.sender[i+1].dmx_data = data[i]
 
 
     def send_frame(self, frame: np.array):
