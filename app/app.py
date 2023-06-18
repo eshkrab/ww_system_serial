f4570d3ca39fa0f9c3989f71c08b8ae377376999
rina shkrabova <eshkrab@gmail.com>
2023-06-18 12:46:53 -0700

    zmq pubsub

--- app/app.py
+++ app/app.py
@@ -1,39 +1,102 @@
 import json
 import logging
 import serial
+import zmq
 import zmq.asyncio
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
+class Player:
+    def __init__(self, brightness=250.0, fps=30, state="stopped", mode="repeat", current_media=None):
+        self.state = state
+        self.mode = mode
+        self.brightness = brightness
+        self.fps = fps
+        self.current_media = current_media
 
-# Load configuration from JSON file
-with open('config/config.json', 'r') as f:
-    config = json.load(f)
 
-# Set up ZeroMQ context and socket
-ctx = zmq.asyncio.Context()
-socket = ctx.socket(zmq.REQ)
-socket.connect(f"tcp://{config['zmq']['ip_server']}:{config['zmq']['port']}")
+############################
+# CONFIG
+############################
+def load_config(config_file):
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+    return config
 
-# Set up Serial port
+def get_log_level( level):
+    levels = {
+        'DEBUG': logging.DEBUG,
+        'INFO': logging.INFO,
+        'WARNING': logging.WARNING,
+        'ERROR': logging.ERROR,
+        'CRITICAL': logging.CRITICAL
+    }
+    return levels.get(level.upper(), logging.INFO)
+
+config = load_config('config/config.json')
+
+logging.basicConfig(level=get_log_level(config['debug']['log_level']))
+
+############################
+# PLAYER
+############################
+player = Player(brightness=config['brightness_level'], fps=config['fps'])
+
+########################
+# SERIAL
+########################
 serial_port = config['serial_port']
 serial_baudrate = config['serial_baudrate']
 ser = serial.Serial(serial_port, serial_baudrate)
 
+########################
+# ZMQ
+########################
+ctx = zmq.asyncio.Context()
+# Publish to the player app
+pub_socket = ctx.socket(zmq.PUB)
+pub_socket.bind(f"tcp://{config['zmq']['ip_bind']}:{config['zmq']['port_server_pub']}")  # Publish to the player app
+
+# Subscribe to the player app
+sub_socket = ctx.socket(zmq.SUB)
+sub_socket.connect(f"tcp://{config['zmq']['ip_connect']}:{config['zmq']['port_player_pub']}")  
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
 async def send_message_to_player(message):
     try:
-        logging.debug(f"Sending message: {message}")
-        await socket.send_string(message)
-        reply = await socket.recv_string()
-        return reply
+        logging.debug(f"Publishing message: {message}")
+        await pub_socket.send_string(message)
     except zmq.ZMQError as e:
-        logging.error(f"ZMQError while sending/receiving message: {e}")
+        logging.error(f"ZMQError while publishing message: {e}")
         return -1
+
+async def subscribe_to_player():
+    while True:
+        message = await sub_socket.recv_string()
+        logging.debug(f"Received from Player: {message}")
+
+        # Process the received message
+        message = message.split(" ")
+        if message[0] == "state":
+            player.state = message[1]
+        elif message[0] == "mode":
+            player.mode = message[1]
+        elif message[0] == "brightness":
+            brightness = float(message[1]) / 255.0
+            player.brightness = float(brightness)
+        elif message[0] == "fps":
+            player.fps = int(message[1])
+        elif message[0] == "current_media":
+            player.current_media = message[1]
+        else:
+            logging.error(f"Unknown message from Player: {message}")
+
+        await asyncio.sleep(0.05)
+
 
 async def handle_zmq_to_serial():
     while True:
-        message = await socket.recv_string()
-        logging.debug(f"Received message from ZeroMQ: {message}")
+        #  message = await socket.recv_string()
+        message = "serial test"
+        #  logging.debug(f"Received message from ZeroMQ: {message}")
         
         # Write the message to the serial port
         ser.write(message.encode())
@@ -46,11 +109,13 @@ async def handle_serial_to_zmq():
 
             # Process the data or send it to ZeroMQ
             # Example: Send the data as a message to ZeroMQ
-            reply = await send_message_to_player(f"process_data {data}")
+            await send_message_to_player(f"process_data {data}")
             logging.debug(f"Reply from player: {reply}")
 
 # Example usage
 async def example_usage():
+    # Start listening to messages from player app
+    asyncio.ensure_future(subscribe_to_player())
     # Start the ZeroMQ-to-Serial and Serial-to-ZeroMQ handlers
     tasks = [
         handle_zmq_to_serial(),
