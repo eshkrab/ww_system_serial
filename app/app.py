b7af34dfe08ff93cd90ae313ee85de4fc2f70eb7
rina shkrabova <eshkrab@gmail.com>
2023-06-20 11:52:19 -0700

    sub only

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import asyncio
 import time
 import zmq
 import zmq.asyncio
-from modules.zmqcomm import subscribe_to_messages
+#  from modules.zmqcomm import subscribe_to_messages
 
 
 class Player:
@@ -59,6 +59,24 @@ ctx = zmq.asyncio.Context()
 pub_socket = ctx.socket(zmq.PUB)
 pub_socket.bind(f"tcp://{config['zmq']['ip_bind']}:{config['zmq']['port_serial_pub']}")  # Publish to the player app
 
+async def subscribe_to_messages( ip_connect, port, process_message):
+    logging.info("Started listening to messages")
+
+    ctx = zmq.asyncio.Context.instance()
+    sub_sock = ctx.socket(zmq.SUB)
+    sub_sock.connect(f"tcp://{ip_connect}:{port}")
+    sub_sock.setsockopt_string(zmq.SUBSCRIBE, "")
+    logging.debug("socket port: "+ str(sub_sock.getsockopt(zmq.LAST_ENDPOINT)))
+
+    try:
+        while True:
+            logging.debug("Waiting for message")
+            message = await sub_sock.recv_string()
+            logging.debug("Received message: " + message)
+            process_message(message)
+    finally:
+        sub_sock.setsockopt(zmq.LINGER, 0)
+        sub_sock.close()
 
 async def send_message_to_player(message):
     try:
