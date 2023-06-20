444c0289f3a18ed6791e142c98b04df00b75f036
rina shkrabova <eshkrab@gmail.com>
2023-06-20 07:45:00 -0700

    cleaning up modules

--- app/app.py
+++ app/app.py
@@ -125,12 +125,14 @@ async def main():
     # Start listening to messages from player app and monitor the socket
     tasks = [
         asyncio.create_task(listen_to_messages(p_sub_sock, process_message)),
-    ]
-
-    tasks.extend([
         asyncio.create_task(handle_zmq_to_serial()),
         asyncio.create_task(handle_serial_to_zmq())
-    ])
+    ]
+
+    #  tasks.extend([
+    #      asyncio.create_task(handle_zmq_to_serial()),
+    #      asyncio.create_task(handle_serial_to_zmq())
+    #  ])
 
     logging.debug("Tasks created")
 
